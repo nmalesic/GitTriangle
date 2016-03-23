c0c1b3e8fbe74323365bd1cd2837a708db65eb9d
fcoeuret <fabrice.coeuret@berger-levrault.fr>
2016-03-23 16:03:08 +0100

    Ajout test isEquilateral

---
+++ src/bl/triangle/EquilateralTest.java
@@ -0,0 +1,41 @@
+package bl.triangle;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class EquilateralTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+		
+	}
+	
+	@Test
+    public void isEquilateralTest() {
+        int ligne1 = 5;
+        int ligne2 = 5;
+        int ligne3 = 5;
+        Equilateral triangle = new Equilateral();
+        boolean result = triangle.isEquilateral(ligne1,ligne2,ligne3);
+        assertTrue(result);;
+    }
+
+	@Test
+    public void isNotEquilateralTest() {
+        int ligne1 = 8;
+        int ligne2 = 6;
+        int ligne3 = 5;
+        Equilateral triangle = new Equilateral();
+        boolean result = triangle.isEquilateral(ligne1,ligne2,ligne3);
+        assertFalse(result);;
+    }
+   
+	
+}
