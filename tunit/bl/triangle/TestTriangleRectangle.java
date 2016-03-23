946c641cb477e337c7f7a0340785f886d4393978
nmalesic <nicolas.malesic@Berger-Levraul.fr>
2016-03-23 16:33:52 +0100

    tu rectangle

--- tunit/bl/triangle/TestTriangleRectangle.java
+++ tunit/bl/triangle/TestTriangleRectangle.java
@@ -2,13 +2,28 @@ package bl.triangle;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestTriangleRectangle {
 
+	Triangle tRectangle;
+	Triangle tPasRectangle;
+	
+	@Before
+	public void init() {
+		tRectangle = new Triangle(3,4,5);
+		tPasRectangle = new Triangle(2,3,4);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRectangle() {
+		assertTrue("Triangle rectangle", tRectangle.isRectangle());
+	}
+
+	@Test
+	public void testPasRectangle() {
+		assertFalse("Triangle rectangle", tPasRectangle.isRectangle());
 	}
 
 }
